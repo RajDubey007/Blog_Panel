a8cb4fac9c6d2caf315f36c74d1c70fdaec95ec1
RajDubey007 <raj7984175615>
2024-10-04 21:17:22 +0530

    Blog_Panel

---
+++ controllers/blogCon.js
@@ -0,0 +1,21 @@
+const blogModel = require('../models/blogModel');
+
+const blog = async (req, res) => {
+
+    let blogData = await blogModel.find();
+    // console.log("blogData", blogData);
+
+
+    res.render('blog', { userImg: req.user.path, fname: req.user.fname, lname: req.user.lname, email: req.user.email, blogData: blogData });
+
+}
+
+const myblog = async (req, res) => {
+
+    let blogData = await blogModel.find({ user_id: req.user._id });
+    // console.log("blogData", blogData);
+
+    res.render('myblog', { userImg: req.user.path, fname: req.user.fname, lname: req.user.lname, email: req.user.email, blogData: blogData });
+}
+
+module.exports = { blog, myblog };
---
+++ models/blogModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    path: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: String, required: true, default: new Date() },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+});
+
+module.exports = mongoose.model('blog', blogSchema);
