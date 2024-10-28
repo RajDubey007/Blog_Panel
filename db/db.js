b9b8768dd5213aa5241c547b61e9f022dc45415a
RajDubey007 <raj7984175615@gmail.com>
2024-10-28 12:58:19 +0530

    node

--- db/db.js
+++ db/db.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Blog_Data')
+mongoose.connect('mongodb://localhost:27017/Node_Data')
     .then(() => console.log('Connected to database'))
     .catch((err) => console.log(err));
+
+
+// mongodb+srv://rajd07:<rajd07>@cluster6.daslz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster6
+
+// git config --global user.name "Your Name"
+// git config --global user.email "your.email@example.com"
 
 module.exports = { mongoose }; 
