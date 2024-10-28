ad7e3b58eb2e0b0f8146126d0c067d901b486661
RajDubey007 <raj7984175615@gmail.com>
2024-10-28 13:04:28 +0530

    node p

--- db/db.js
+++ db/db.js
@@ -10,4 +10,10 @@ mongoose.connect('mongodb://localhost:27017/Node_Data')
 // git config --global user.name "Your Name"
 // git config --global user.email "your.email@example.com"
 
+
+//mongodb+srv://rajd07:<rajd07>@cluster6.daslz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster6
+
+// git config --global user.name "Your Name"
+// git config --global user.email "your.email@example.com"
+
 module.exports = { mongoose }; 
